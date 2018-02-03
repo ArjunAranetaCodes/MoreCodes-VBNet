49827a4d31f2f192577d372bd948720801de164f
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-03 09:05:01 +0800

    2018 Updates

--- Conversions/problem2.vb
+++ Conversions/problem2.vb
@@ -2,22 +2,10 @@
 Module Module1
 
     Sub Main()
-        Dim num1 As Integer = 0
-        Dim num2 As Integer = 0
-        Console.Write("Enter value of num1: ")
-        num1 = Int32.Parse(Console.ReadLine())
-        Console.Write("Enter value of num2: ")
-        num2 = Int32.Parse(Console.ReadLine())
-        Dim sum As Integer = num1 + num2
-        Dim diff As Integer = num1 - num2
-        Dim prod As Integer = num1 * num2
-        Dim quot As Integer = num1 / num2
-
-        Console.WriteLine("Sum is " & (sum))
-        Console.WriteLine("Difference is " & (diff))
-        Console.WriteLine("Product is " & (prod))
-        Console.WriteLine("Quotient is " & (quot))
+        Dim num As String = "10"
+        Console.WriteLine(Integer.Parse(num))
         Console.ReadKey()
     End Sub
 
 End Module
+
---
+++ Conversions/problem3.vb
@@ -0,0 +1,10 @@
+'Problem 3: Write a program that converts an integer to float.
+Module Module1
+
+    Sub Main()
+        Dim num As Integer = 10
+        Console.WriteLine(Double.Parse(num))
+        Console.ReadKey()
+    End Sub
+
+End Module
