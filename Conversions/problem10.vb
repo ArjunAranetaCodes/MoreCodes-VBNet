d6fedddeb8c38e4ae951b5f32de90ca018613a3e
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-16 07:08:24 +0800

    2018 Updates

--- Conversions/problem10.vb
+++ Conversions/problem10.vb
@@ -2,21 +2,15 @@
 Module Module1
 
     Sub Main()
-        Dim num1 As Integer = 0
-        Dim num2 As Integer = 0
-        Dim num3 As Integer = 0
-        Dim ave As Integer = 0
+        Dim dec As Integer = 256
+        Dim oct As String = ""
 
-        Console.Write("Enter value of num1: ")
-        num1 = Int32.Parse(Console.ReadLine())
-        Console.Write("Enter value of num2: ")
-        num2 = Int32.Parse(Console.ReadLine())
-        Console.Write("Enter value of num3: ")
-        num3 = Int32.Parse(Console.ReadLine())
-        ave = (num1 + num2 + num3) / 3
+        Do While dec > 0
+            oct = (dec Mod 8).ToString & oct
+            dec = Math.Floor(dec / 8)
+        Loop
 
-        Console.WriteLine("Average is " & ave)
-
+        Console.WriteLine(oct)
         Console.ReadKey()
     End Sub
 
