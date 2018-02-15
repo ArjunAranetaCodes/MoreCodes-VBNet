d99c4e2b805fae960e6fee66b3484551f6e18a0a
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-16 07:07:46 +0800

    2018 Updates

---
+++ Conversions/problem11.vb
@@ -0,0 +1,12 @@
+'Problem 11: Write a program that converts numbers of day to minutes.
+Module Module1
+
+    Sub Main()
+        Dim day As Integer = 1
+        Dim minute As Integer = day * 24 * 60
+
+        Console.WriteLine(minute)
+        Console.ReadKey()
+    End Sub
+
+End Module
