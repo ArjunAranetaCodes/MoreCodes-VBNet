df6d2bd5da4d4a764b2e97ec3a04100203dad9e1
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-17 08:27:51 +0800

    2018 Updates

---
+++ Conversions/problem12.vb
@@ -0,0 +1,12 @@
+'Problem 12: Write a program that converts numbers of day to seconds.
+Module Module1
+
+    Sub Main()
+        Dim day As Integer = 1
+        Dim seconds As Integer = day * 24 * 60 * 60
+
+        Console.WriteLine(seconds)
+        Console.ReadKey()
+    End Sub
+
+End Module
