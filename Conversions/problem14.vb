9c0a44c0d2124064a854062fb2b671fd1f7b54a3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-20 07:20:00 +0800

    2018 Updates

---
+++ Conversions/problem14.vb
@@ -0,0 +1,12 @@
+'Problem 14: Write a program that converts lower case to upper case characters.
+Module Module1
+
+    Sub Main()
+        Dim str As String = "abc"
+        str = str.ToUpper()
+
+        Console.WriteLine(str)
+        Console.ReadKey()
+    End Sub
+
+End Module
