fed0144697aae132bf699360bb88571d84d18fee
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-21 05:58:56 +0800

    2018 Updates

---
+++ Conversions/problem15.vb
@@ -0,0 +1,12 @@
+'Problem 15: Write a program that converts letters to ASCII.
+Module Module1
+
+    Sub Main()
+        Dim letter As String = "x"
+        Dim ascii As Integer = Asc(letter)
+
+        Console.WriteLine(ascii)
+        Console.ReadKey()
+    End Sub
+
+End Module
