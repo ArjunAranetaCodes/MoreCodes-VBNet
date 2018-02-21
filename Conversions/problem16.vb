429f96b9200b63ab325e53caa6dbbef9d5305edf
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-22 05:50:21 +0800

    2018 Updates

---
+++ Conversions/problem16.vb
@@ -0,0 +1,12 @@
+'Problem 16: Write a program that converts feet to inch.
+Module Module1
+
+    Sub Main()
+        Dim feet As Integer = 1
+        Dim inch As Integer = feet * 12
+
+        Console.WriteLine(inch)
+        Console.ReadKey()
+    End Sub
+
+End Module
