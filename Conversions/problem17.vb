fb39c7b42ae8f5c89387526fe40a9372eff3f58f
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-23 05:43:26 +0800

    2018 Updates

---
+++ Conversions/problem17.vb
@@ -0,0 +1,12 @@
+'Problem 17: Write a program that converts kilometer to meter.
+Module Module1
+
+    Sub Main()
+        Dim kilometer As Integer = 1
+        Dim meter As Integer = kilometer * 1000
+
+        Console.WriteLine(meter)
+        Console.ReadKey()
+    End Sub
+
+End Module
