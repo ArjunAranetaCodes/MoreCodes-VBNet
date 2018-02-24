bbc687991efcd82802bdf49bcf7c3231aa206933
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-24 08:44:17 +0800

    2018 Updates

---
+++ Conversions/problem18.vb
@@ -0,0 +1,12 @@
+'Problem 18: Write a program that converts liter to milliliter.
+Module Module1
+
+    Sub Main()
+        Dim liter As Integer = 1
+        Dim milliliter As Integer = liter * 1000
+
+        Console.WriteLine(milliliter)
+        Console.ReadKey()
+    End Sub
+
+End Module
