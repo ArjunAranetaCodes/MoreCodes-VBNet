b9ffe6f54f905eaad1b780f7b08ea69fae8ac28f
Arjun Araneta <arjunaraneta@gmail.com>
2018-12-10 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem3.vb
@@ -0,0 +1,24 @@
+﻿'Problem 3: Write a program to print the number pattern of ones and zeros using loop.
+'01010
+'01010
+'01010
+'01010
+'01010
+Module Module1
+
+    Sub Main()
+        For y As Integer = 0 To 4
+            For x As Integer = 0 To 4
+                If x Mod 2 = 0 Then
+                    Console.Write("0")
+                Else
+                    Console.Write("1")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
